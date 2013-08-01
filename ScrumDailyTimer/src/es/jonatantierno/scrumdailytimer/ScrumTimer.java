770d632a9597908e668c02b4f31273cb9fcff2d6
Jonatan Tierno <jta.javato@gmail.com>
2013-08-01 15:24:24 +0200

    Flow with taps in activity. No timer yet

---
+++ ScrumDailyTimer/src/es/jonatantierno/scrumdailytimer/ScrumTimer.java
@@ -0,0 +1,15 @@
+
+package es.jonatantierno.scrumdailytimer;
+
+/**
+ * This class implements a timer, with a precision of seconds, and provides the content for both the total meeting time
+ * and the countdown.
+ * 
+ * @author jonatantierno
+ */
+public class ScrumTimer {
+    public void start() {
+
+    }
+
+}
